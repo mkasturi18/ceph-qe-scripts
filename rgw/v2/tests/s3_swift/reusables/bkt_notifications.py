2c5fc09eda54e056e6d196af1782b1134efd637e
MadhaviKasturi <mkasturi@redhat.com>
2021-12-11 08:33:40 -0500

    change path
    Signed-off-by: MadhaviKasturi <mkasturi@redhat.com>

--- rgw/v2/tests/s3_swift/reusables/bkt_notifications.py
+++ rgw/v2/tests/s3_swift/reusables/bkt_notifications.py
@@ -28,16 +28,16 @@ def start_kafka_broker_consumer(topic_name, event_record_path, ceph_version):
         t = utils.exec_shell_cmd(cmd) 
         print("path exists :",t)
 
-    path = "$KAFKA_HOME" #'/home/cephuser/kafka*/'
+#    path = utils.exec_shell_cmd("echo $KAFKA_HOME)" #'/home/cephuser/kafka*/'
 
     #start zookeeper and kafka broker
     #start_kafka_broker(path)
 
     # start kafka consumer
     if "nautilus" in ceph_version:
-        cmd = f"{path}/bin/kafka-console-consumer.sh --bootstrap-server kafka://localhost:9092 --from-beginning --topic {topic_name} --timeout-ms 30000 >> {event_record_path}"
+        cmd = f"$KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server kafka://localhost:9092 --from-beginning --topic {topic_name} --timeout-ms 30000 >> {event_record_path}"
     else:
-        cmd = f"{path}/bin/kafka-console-consumer.sh --bootstrap-server kafka://localhost:9092 --from-beginning --topic {topic_name} --zookeeper localhost:2181 --timeout-ms 30000 >> {event_record_path}"
+        cmd = f"$KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server kafka://localhost:9092 --from-beginning --topic {topic_name} --zookeeper localhost:2181 --timeout-ms 30000 >> {event_record_path}"
     start_consumer_kafka = utils.exec_shell_cmd(cmd) 
 
     #stop kafka broker
